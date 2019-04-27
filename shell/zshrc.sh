4bdb2e64f6cc06a25208edeff1fc6113f6a5d2d3
Fernando Correia <fernandoacorreia@gmail.com>
2019-04-27 13:13:57 -0700

    Enable antigen

--- shell/zshrc.sh
+++ shell/zshrc.sh
@@ -1,14 +1,14 @@
 DEFAULT_USER=$(whoami)
 
-#source $HOME/homefiles/vendor/antigen/antigen.zsh
-#antigen use oh-my-zsh
+source $HOME/macfiles/vendor/antigen/antigen.zsh
+antigen use oh-my-zsh
 
-#antigen bundle git
-#antigen bundle command-not-found
-#antigen bundle zsh-users/zsh-completions
-#antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle git
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-syntax-highlighting
 
-#antigen theme agnoster
-#antigen apply
+antigen theme agnoster
+antigen apply
 
 source ~/.profile
