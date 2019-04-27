39bef40daa59896b9f9810be992f02d4e60def2b
Fernando Correia <fernandoacorreia@gmail.com>
2019-04-27 13:50:23 -0700

    Install ripgrep

--- shell/environment.sh
+++ shell/environment.sh
@@ -1,6 +1,6 @@
 # fzf
 export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history"
-# export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
 # sbt
 export SBT_OPTS="-Xss256m -Xmx4g"
