8658792af7cd753fc4a8da7286f90db3c3fea791
Fernando Correia <fernandoacorreia@gmail.com>
2019-04-27 13:54:28 -0700

    Set less options

--- shell/environment.sh
+++ shell/environment.sh
@@ -2,6 +2,9 @@
 export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history"
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
+# less command options
+export LESS='--quit-if-one-screen --no-init --RAW-CONTROL-CHARS'
+
 # sbt
 export SBT_OPTS="-Xss256m -Xmx4g"
 
