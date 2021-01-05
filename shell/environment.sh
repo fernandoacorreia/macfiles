07d02c70205ea03a6a91707e9884b05ee70614d6
Fernando Correia <fernando@dominodatalab.com>
2021-01-04 16:08:29 -0800

    Install Golang

--- shell/environment.sh
+++ shell/environment.sh
@@ -24,3 +24,8 @@ export PATH="$HOME/macfiles/bin:$PATH"
 
 # Add gnubin to PATH
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+
+# Golang
+export GOPATH="$HOME/go"
+export GOROOT="/usr/local/opt/go/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
